9170384fb3d0c2362706a9bb2016ddda077d904d
st10451478-Khaya-Nxumalo <ST10451478@vcconnect.edu.za>
2025-08-27 23:34:26 +0200

    Initial setup of services

---
+++ ABCRetail/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ABCRetail.Models
+{
+    public class CustomerEntity : ITableEntity
+    {
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+        
+        public string? PhotoUrl { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public string? Address { get; set; }    
+    }
+}
